fc4882d7b4a6e169f1a58edac15b9f2c95ceb945
Artem Paskal <artem.yogi@gmail.com>
2022-06-29 15:18:35 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const colorToggle = document.querySelectorAll(['.shop__colors']);
+
+colorToggle.forEach(toggle => toggle.addEventListener('click', () => {
+  const sofas = document.querySelectorAll(['.shop__sofa-svg']);
+
+  const selectedColor = window.getComputedStyle(toggle).backgroundColor;
+  const opacity = window.getComputedStyle(toggle).opacity;
+
+  const selectedColorRGBA
+  = `${selectedColor.slice(0, 3)}a${selectedColor.slice(3, -1)}, ${opacity})`;
+
+  sofas.forEach(sofa => {
+    sofa.style.fill = selectedColorRGBA;
+  });
+}));
+
+const colorSelectors = document.querySelectorAll(['.shop__colors-wraper']);
+
+colorSelectors.forEach(selector =>
+  selector.addEventListener('click', function() {
+    const current
+    = document.getElementsByClassName('shop__colors-wraper--is-active');
+
+    current[0].className
+    = current[0].className.replace(' shop__colors-wraper--is-active', '');
+    this.className += ' shop__colors-wraper--is-active';
+  }));
+
+document.getElementById('mail-form').addEventListener(
+  'submit', function(event) {
+    event.preventDefault();
+  }, false
+);
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
